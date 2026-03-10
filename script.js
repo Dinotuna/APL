173a4502765a9ae95639b4033b9b37e7ce969400
dinotuna <dino.omicevic@elev.ga.ntig.se>
2026-03-10 13:20:55 +0900

    Added the ability to choose a wave and play it in the browser

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+let mysound = null;
+
 async function main() {
   await Tone.start();
 
-  mysound = sine_wave();
+  const selectedWave = document.getElementById("selectWave").value;
+
+  if (selectedWave === "sineWave") {
+    mysound = sine_wave();
+  }
+  else if (selectedWave === "triangleWave") {
+    mysound = triangle_wave();
+  }
+  else if (selectedWave === "squareWave") {
+    mysound = square_wave();
+  }
+
+  else if (selectedWave === "sawtoothWave") {
+    mysound = sawtooth_wave();
+  }
+
+  else {
+    console.log("Select a wave")
+    return;
+  }
+
+
   mysound.connect(Tone.Destination);
 }
 
@@ -15,7 +38,6 @@ document.getElementById("playButton").addEventListener("click", () => {
 document.getElementById("stopButton").addEventListener("click", () => {
   if (mysound) {
     mysound.stop();
-    console.log("Sound Stopped");
   }
 });
 
@@ -40,12 +62,12 @@ function triangle_wave(frequency = 440, amplitude = 0.5) {
     frequency: frequency,
     volume: Tone.gainToDb(amplitude),
   }).start();
-
-  function square_wave(frequency = 440, amplitude = 0.5) {
+}
+function square_wave(frequency = 440, amplitude = 0.5) {
     return new Tone.Oscillator({
       type: "square",
       frequency: frequency,
       volume: Tone.gainToDb(amplitude),
     }).start();
   }
-}
+
